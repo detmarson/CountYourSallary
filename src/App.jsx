2c21a80b76e7479e17a810c1c75cd0839d56738f
Marcin Detlaf <marcin.detlaf7@gmail.com>
2023-09-21 16:02:30 +0200

    Added autosave to the calendar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import '@coreui/coreui/dist/css/coreui.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { CContainer, CRow, CCol, CFormCheck, CForm, CFormInput, CButton, CInputGroup } from '@coreui/react'
 function App() {
-	const [ratePerHour, setRatePerHour] = useState(0)
+	const [ratePerHour, setRatePerHour] = useState(parseFloat(localStorage.getItem('ratePerHour')) || '')
+	const [hoursWorked, setHoursWorked] = useState('')
 	const [displaySalary, setDisplaySalary] = useState(0)
-	const [calendar, setCalendar] = useState(Calendar())
+	const [calendar, setCalendar] = useState(() => {
+		const storedCalendar = JSON.parse(localStorage.getItem('calendar'))
+		return storedCalendar || Calendar()
+	})
 	function Calendar() {
 		const arrayOfObjects = []
-		for (let id = 1; id <= 30; id++) {
+		for (let id = 1; id <= 31; id++) {
 			const obj = {
 				id: id,
 				hours: 0,
@@ -18,42 +22,59 @@ function App() {
 		}
 		return arrayOfObjects
 	}
+	useEffect(() => {
+		const totalHours = calendar.reduce((acc, day) => acc + day.hours, 0)
+		const totalSalary = ratePerHour * totalHours
+		setDisplaySalary(totalSalary.toFixed(2))
+	}, [calendar, ratePerHour])
+
+	useEffect(() => {
+		localStorage.setItem('ratePerHour', ratePerHour.toString())
+	}, [ratePerHour])
+
+	useEffect(() => {
+		localStorage.setItem('calendar', JSON.stringify(calendar))
+	}, [calendar])
+
+	useEffect(() => {
+		const totalHours = calendar.reduce((acc, day) => acc + day.hours, 0)
+		const totalSalary = ratePerHour * totalHours
+		setDisplaySalary(totalSalary.toFixed(2))
+	}, [calendar, ratePerHour])
 
 	const handleCheck = id => e => {
 		const checked = e.target.checked
 		setCalendar(prevCalendar => prevCalendar.map(day => (day.id === id ? { ...day, checked } : day)))
 	}
 	const handleChangeHours = e => {
+		setHoursWorked(e.target.value)
 		setCalendar(calendar.map(day => (day.checked ? { ...day, hours: parseInt(e.target.value) } : day)))
 	}
-	const countSalary = e => {
+
+	const handleSubmit = e => {
 		e.preventDefault()
-		const totalHours = calendar.reduce((acc, day) => acc + day.hours, 0)
-		const totalSalary = ratePerHour * totalHours
-		setDisplaySalary(totalSalary)
+		setCalendar(calendar.map(day => ({ ...day, checked: false })))
+		setHoursWorked('')
+	}
+	const handleClear = () => {
+		setCalendar(calendar.map(day => ({ ...day, hours: 0 })))
+		setRatePerHour('')
+		setHoursWorked('')
 	}
 	return (
 		<CContainer>
 			<h1>CountYourSallary</h1>
-			<CForm className="row mb-3" onSubmit={countSalary}>
-				<CCol xs="auto">
-					<CFormInput
-						type="number"
-						placeholder="Rate per hour"
-						onChange={e => setRatePerHour(parseFloat(e.target.value))}
-						aria-describedby="button-addon1"
-					/>
-				</CCol>
-				<CCol>
-					<CButton className="col-3" type="submit" color="primary">
-						Count
-					</CButton>
-				</CCol>
-			</CForm>
+			<CFormInput
+				type="number"
+				placeholder="Rate per hour"
+				onChange={e => setRatePerHour(parseFloat(e.target.value))}
+				aria-describedby="button-addon1"
+				value={ratePerHour}
+			/>
 			<hr />
-			<CRow>
+			<CRow xs={{ cols: 6 }}>
 				{calendar.map(day => (
-					<CCol className="m-4" xs="1" key={day.id}>
+					<CCol key={day.id} className="mb-5">
 						<CFormCheck
 							button={{ color: 'primary', variant: 'outline' }}
 							id={day.id}
@@ -68,13 +89,22 @@ function App() {
 			<hr />
 			<CRow className="mb-3">
 				<CCol>
-					<CInputGroup>
-						<CFormInput type="number" placeholder="Amount of hours worked" onChange={handleChangeHours} />
-						<CButton>Deselect</CButton>
-					</CInputGroup>
+					<CForm onSubmit={e => handleSubmit(e)}>
+						<CInputGroup>
+							<CFormInput
+								type="number"
+								placeholder="Amount of hours worked"
+								onChange={handleChangeHours}
+								value={hoursWorked}
+							/>
+							<CButton type="submit">Submit</CButton>
+						</CInputGroup>
+					</CForm>
 				</CCol>
 				<CCol>
-					<CButton>Clear ALL</CButton>
+					<CButton color="danger" onClick={handleClear}>
+						Clear ALL
+					</CButton>
 				</CCol>
 			</CRow>
 			<div>Total Salary: {displaySalary}</div>
